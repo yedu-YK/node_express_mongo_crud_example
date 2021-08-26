80f3a37c067d761fd3adcfeec8ddec7244c2c9c7
yedu krishnan <smyedukrishnan@gmail.com>
2021-08-26 07:40:24 +0530

    connected_to_db

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,9 @@ import express from "express";
 //importing routes
 import routes from "./routes";
 
+//connecting to db
+const mongoose = require("mongoose");
+
 //initializing
 dotenv.config();
 const app = express();
@@ -12,7 +15,16 @@ const app = express();
 //initializing port
 const port = process.env.SERVER_PORT;
 
-app.use("/api",routes)
+//connecting to db server
+mongoose.connect("mongodb://localhost:27017/crud", (error:any) => {
+  if (!error) {
+    console.log("successfully connected to db");
+  } else {
+    console.log("failed to connect db");
+  }
+});
+
+app.use("/api", routes);
 
 app.listen(port, () => {
   console.log(`running at port ${port}`);
--- todo.md
+++ todo.md
@@ -1,7 +1,21 @@
 
-Create Student, Teacher & Class
+Create Student, Teacher & Class 
+    routes-*done*
+    controller-empty functions
+    models
+    db connection
+
 Update Student, Teacher & Class
+    routes-*done*
+    controller-empty functions
+    models
+    db connection
+
 Delete Student, Teacher & Class
+    routes-*done*
+    controller-empty functions
+    db connections
+
 
 Read the list of Students in a division
 Read the list of classes of a teacher.
@@ -10,3 +24,5 @@ Read the divisions of each standard.
 Student (name, roll no, & mobile No)
 Teacher (name, employee id, subject)
 Class (standard, division)
+
+DOCUMENTATION
